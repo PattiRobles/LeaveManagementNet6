a7581613e3f252900a15ef294d967f5b352a565d
Patricia Robles <patriciarobles.tech@gmail.com>
2023-05-22 16:27:43 +0100

    Add ViewModel for LeaveTypes & finetune POST request for create leave type

---
+++ LeaveManagementWeb/Configurations/MapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework.Profiler;
+using AutoMapper;
+using LeaveManagementWeb.Data;
+using LeaveManagementWeb.Models;
+
+namespace LeaveManagementWeb.Configurations
+{
+    public class MapperConfig : Profile 
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+            //allows conversion between types, from LeaveType to LeaveTypeVM and viceversa
+        }
+
+    }
+}
---
+++ LeaveManagementWeb/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementWeb.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Category")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Total Available Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage ="Please enter a value within range: 1 - 25")]
+        public int DefaultDays { get; set; }
+    }
+}
